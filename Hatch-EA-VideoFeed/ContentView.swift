2d4a3457a9b31d774c8ed0db33ea4ef4b06ac927
Sina Reza <40793075+sina3@users.noreply.github.com>
2025-09-24 22:01:12 -0400

    Initial Commit

---
+++ Hatch-EA-VideoFeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hatch-EA-VideoFeed
+//
+//  Created by Sina Rezazadeh on 2025-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hatch-EA-VideoFeed/Hatch_EA_VideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hatch_EA_VideoFeedApp.swift
+//  Hatch-EA-VideoFeed
+//
+//  Created by Sina Rezazadeh on 2025-09-24.
+//
+
+import SwiftUI
+
+@main
+struct Hatch_EA_VideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
